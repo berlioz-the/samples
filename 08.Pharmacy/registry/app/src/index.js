dbc4dd91c6720a488cadc42ce57fed1a4e038f2c
Ruben Hakopian <ruben.hakopian@gmail.com>
2018-05-18 01:10:25 -0700

    working on pharmacy sample

---
+++ 08.Pharmacy/registry/app/src/index.js
@@ -0,0 +1,89 @@
+const AWS = require('aws-sdk');
+const berlioz = require('berlioz-connector');
+const express = require('express')
+const bodyParser = require('body-parser');
+
+const app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (request, response) => {
+    response.send({ service: process.env.BERLIOZ_SERVICE,
+                    id: process.env.BERLIOZ_TASK_ID });
+})
+
+app.post('/item', (request, response) => {
+    var dbInfo = berlioz.getDatabaseInfo('drugs');
+    var docClient = new AWS.DynamoDB.DocumentClient(dbInfo.config);
+
+    var params = {
+        TableName: dbInfo.tableName,
+        Item: {
+            'name': request.body.name
+        }
+    };
+    docClient.put(params, (err, data) => {
+        if (err) {
+            response.send(err);
+        } else {
+            response.send(data);
+        }
+    });
+});
+
+app.get('/items', (request, response) => {
+    var dbInfo = berlioz.getDatabaseInfo('drugs');
+    var docClient = new AWS.DynamoDB.DocumentClient(dbInfo.config);
+
+    var params = {
+        TableName: dbInfo.tableName
+    };
+    docClient.scan(params, (err, data) => {
+        if (err) {
+            response.send(err);
+        } else {
+            response.send(data.Items);
+        }
+    });
+});
+
+// app.get('/peers', (req, response) => {
+//     response.setHeader('Content-Type', 'application/json');
+//     response.send(JSON.stringify(berlioz.extractRoot()));
+// })
+//
+// app.get('/db', (req, response) => {
+//     var dbInfo = berlioz.getDatabaseInfo('recipes');
+//     var docClient = new AWS.DynamoDB.DocumentClient(dbInfo.config);
+//
+//     var params = {
+//         TableName: dbInfo.tableName,
+//         Item: {
+//             'myId': '1234',
+//             'full_name': 'zzasdfasdf',
+//             'eee': 'ddd'
+//         }
+//     };
+//     docClient.put(params, (err, data) => {
+//         if (err) {
+//             response.send(err);
+//         } else {
+//             response.send(JSON.stringify(data));
+//         }
+//     });
+// })
+//
+// app.get('/test', (req, response) => {
+//     var data = berlioz.getDatabaseInfo('recipes');
+//     response.setHeader('Content-Type', 'application/json');
+//     response.send(JSON.stringify(data));
+// })
+
+app.listen(process.env.BERLIOZ_LISTEN_PORT_CLIENT,
+           process.env.BERLIOZ_LISTEN_ADDRESS, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+
+    console.log(`server is listening on ${process.env.BERLIOZ_LISTEN_ADDRESS}:${process.env.BERLIOZ_LISTEN_PORT_CLIENT}`)
+})
---
+++ 08.Pharmacy/web/src/index.js
@@ -0,0 +1,133 @@
+const express = require('express')
+const _ = require('lodash')
+const request = require('request-promise');
+const AWS = require('aws-sdk');
+const Promise = require('promise');
+const berlioz = require('berlioz-connector');
+
+const app = express()
+
+app.use(express.static('public'))
+app.set('view engine', 'ejs');
+
+app.get('/', (req, response) => {
+    var renderData = {
+        settings: [
+            {name: 'Task ID', value: process.env.BERLIOZ_TASK_ID },
+            {name: 'Instance ID', value: process.env.BERLIOZ_INSTANCE_ID },
+            {name: 'Region', value: process.env.BERLIOZ_AWS_REGION }
+        ],
+        peers: appClientEndpoints,
+        peersStr: JSON.stringify(appClientEndpoints, null, 2),
+        appPeer: {
+        }
+    };
+
+    return Promise.resolve()
+        // .then(() => queryFromAppClient(renderData.appPeer))
+        .catch(error => {
+            if (error instanceof Error) {
+                renderData.error = error.stack + error.stack;
+            } else {
+                renderData.error = JSON.stringify(error, null, 2);
+            }
+        })
+        .then(() => {
+            response.render('pages/index', renderData);
+        })
+        ;
+})
+//
+//
+// app.get('/peers', (req, response) => {
+//     response.setHeader('Content-Type', 'application/json');
+//     response.send(JSON.stringify(berlioz.extractRoot()));
+// })
+//
+// app.get('/queue/info', (req, response) => {
+//     var kinesisInfo = berlioz.getQueueInfo('messages');
+//     response.send(JSON.stringify(kinesisInfo));
+// })
+//
+// app.get('/queue/insert', (req, response) => {
+//     var kinesisInfo = berlioz.getQueueInfo('messages');
+//     var kinesis = new AWS.Kinesis(kinesisInfo.config);
+//
+//     var params = {
+//         StreamName: kinesisInfo.streamName,
+//         PartitionKey: 'myKey',
+//         Data: 'Hello from ' + process.env.BERLIOZ_TASK_ID
+//     };
+//     return kinesis.putRecord(params).promise()
+//         .then(data => {
+//             response.send(JSON.stringify(data));
+//         })
+//         .catch(reason => {
+//             response.send(err);
+//         });
+// })
+//
+// app.get('/queue/read', (req, response) => {
+//     var kinesisInfo = berlioz.getQueueInfo('messages');
+//     var kinesis = new AWS.Kinesis(kinesisInfo.config);
+//
+//     return kinesis.describeStream({ StreamName: kinesisInfo.streamName }).promise()
+//         .then(streamData => {
+//             var shardId = streamData.StreamDescription.Shards[0].ShardId;
+//             return kinesis.getShardIterator({ ShardId: shardId, StreamName: kinesisInfo.streamName, ShardIteratorType: 'TRIM_HORIZON' }).promise();
+//         })
+//         .then(shardIteratorData => {
+//             return kinesis.getRecords({ ShardIterator: shardIteratorData.ShardIterator }).promise();
+//         })
+//         .then(data => {
+//             response.send(JSON.stringify(data));
+//         })
+//         .catch(reason => {
+//             response.send(reason);
+//         });
+// })
+//
+// app.get('/queue/describe', (req, response) => {
+//     var kinesisInfo = berlioz.getQueueInfo('messages');
+//     var kinesis = new AWS.Kinesis(kinesisInfo.config);
+//
+//     return kinesis.describeStream({ StreamName: kinesisInfo.streamName }).promise()
+//         .then(data => {
+//             response.send(JSON.stringify(data));
+//         })
+//         .catch(reason => {
+//             response.send(reason);
+//         });
+// })
+//
+// function queryFromAppClient(appPeer)
+// {
+//     if (appClientEndpoints && _.keys(appClientEndpoints).length > 0)
+//     {
+//         var peerIdentities = _.keys(appClientEndpoints);
+//         var peerIdentity = peerIdentities[_.random(peerIdentities.length - 1)];
+//         var peer = appClientEndpoints[peerIdentity];
+//         appPeer.url = 'http://' + peer.address + ':' + peer.port;
+//         return request({ url: appPeer.url, json: false, timeout: 5000 })
+//             .then(body => {
+//                 appPeer.cardClass = 'eastern-blue';
+//                 appPeer.title = 'RESPONSE';
+//                 appPeer.response = JSON.stringify(body, null, 2);
+//             })
+//             .catch(error => {
+//                 appPeer.cardClass = 'red';
+//                 appPeer.title = 'ERROR';
+//                 appPeer.response = JSON.stringify(error, null, 2);
+//             });
+//     } else {
+//         appPeer.cardClass = 'yellow';
+//         appPeer.title = 'No peers present';
+//     }
+// }
+
+app.listen(process.env.BERLIOZ_LISTEN_PORT_CLIENT, process.env.BERLIOZ_LISTEN_ADDRESS, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+    console.log(`server is listening on ${process.env.BERLIOZ_LISTEN_ADDRESS}:${process.env.BERLIOZ_LISTEN_PORT_CLIENT}`)
+})
