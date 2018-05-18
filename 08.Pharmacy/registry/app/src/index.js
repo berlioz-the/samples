b9aca3b731e73cfc209844c917099ca6ad89226a
Ruben Hakopian <ruben.hakopian@gmail.com>
2018-05-18 01:10:41 -0700

    pharmacy

--- 08.Pharmacy/registry/app/src/index.js
+++ 08.Pharmacy/registry/app/src/index.js
@@ -47,37 +47,13 @@ app.get('/items', (request, response) => {
     });
 });
 
-// app.get('/peers', (req, response) => {
-//     response.setHeader('Content-Type', 'application/json');
-//     response.send(JSON.stringify(berlioz.extractRoot()));
-// })
-//
-// app.get('/db', (req, response) => {
-//     var dbInfo = berlioz.getDatabaseInfo('recipes');
-//     var docClient = new AWS.DynamoDB.DocumentClient(dbInfo.config);
-//
-//     var params = {
-//         TableName: dbInfo.tableName,
-//         Item: {
-//             'myId': '1234',
-//             'full_name': 'zzasdfasdf',
-//             'eee': 'ddd'
-//         }
-//     };
-//     docClient.put(params, (err, data) => {
-//         if (err) {
-//             response.send(err);
-//         } else {
-//             response.send(JSON.stringify(data));
-//         }
-//     });
-// })
-//
-// app.get('/test', (req, response) => {
-//     var data = berlioz.getDatabaseInfo('recipes');
-//     response.setHeader('Content-Type', 'application/json');
-//     response.send(JSON.stringify(data));
-// })
+app.get('/peers', (req, response) => {
+    response.send(berlioz.extractRoot());
+});
+
+app.get('/env', (req, response) => {
+    response.send(process.env);
+});
 
 app.listen(process.env.BERLIOZ_LISTEN_PORT_CLIENT,
            process.env.BERLIOZ_LISTEN_ADDRESS, (err) => {
