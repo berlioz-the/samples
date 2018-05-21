e32db491f1eb2a1fcbd90ce1fd8a93d65d1a1b67
berlioz <berlioz-cloud@gmail.com>
2018-05-21 11:49:03 -0700

    cleanup in hello world samples.

--- 01.HelloWorld/v4.dns/app/src/index.js
+++ 01.HelloWorld/v4.dns/app/src/index.js
@@ -1,11 +1,14 @@
-// test 1234
+const express = require('express');
 
-const express = require('express')
+const app = express();
 
-const app = express()
 app.get('/', (request, response) => {
-    response.send('Hello from Berlioz App Tier! <br/> App ID: ' + process.env.BERLIOZ_TASK_ID)
+    response.send({
+        message: 'Hello from App Tier',
+        appId: process.env.BERLIOZ_TASK_ID
+    });
 })
+
 app.listen(process.env.BERLIOZ_LISTEN_PORT_CLIENT,
            process.env.BERLIOZ_LISTEN_ADDRESS, (err) => {
     if (err) {
@@ -13,18 +16,4 @@ app.listen(process.env.BERLIOZ_LISTEN_PORT_CLIENT,
     }
 
     console.log(`server is listening on ${process.env.BERLIOZ_LISTEN_ADDRESS}:${process.env.BERLIOZ_LISTEN_PORT_CLIENT}`)
-})
-
-
-const admin_app = express();
-admin_app.get('/', (request, response) => {
-    response.send('Hello from App Admin Tier! <br/> App ID: ' + process.env.BERLIOZ_TASK_ID);
-})
-admin_app.listen(process.env.BERLIOZ_LISTEN_PORT_EXTRA,
-                 process.env.BERLIOZ_LISTEN_ADDRESS, (err) => {
-    if (err) {
-        return console.log('something bad happened', err)
-    }
-
-    console.log(`admin server is listening on ${process.env.BERLIOZ_LISTEN_ADDRESS}:${process.env.BERLIOZ_LISTEN_PORT_EXTRA}`)
-})
+});
