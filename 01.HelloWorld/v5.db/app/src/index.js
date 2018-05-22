3466d850e46ebdaf1d767b127203b663fe672457
berlioz <berlioz-cloud@gmail.com>
2018-05-21 17:48:05 -0700

    workding on mysql sample

--- 01.HelloWorld/v5.db/app/src/index.js
+++ 01.HelloWorld/v5.db/app/src/index.js
@@ -1,6 +1,44 @@
 const express = require('express')
-
 const berlioz = require('berlioz-connector');
+const mysql = require('promise-mysql');
+
+function executeQuery(querySql)
+{
+    var conn = null;
+    var finalResult = null;
+    var peer = berlioz.getRandomPeer('service', 'db', 'client');
+    var options = {
+        host: peer.address,
+        port: peer.port,
+        user: process.env.HELLO_RELATIONAL_DB_USER,
+        password: process.env.HELLO_RELATIONAL_DB_PASS,
+        database: process.env.HELLO_RELATIONAL_DB_NAME,
+        insecureAuth: true
+    };
+    return mysql.createConnection(options)
+        .then(result => {
+            conn = result;
+            return conn.query(querySql);
+        })
+        .then(result => {
+            finalResult = result;
+        })
+        .finally(() => conn.end())
+        .then(() => finalResult);
+}
+
+//
+// return executeQuery('select * from addressBook')
+//     .then(rows => {
+//         console.log(rows);
+//     })
+//     .then(() => executeQuery('select * from addressBook'))
+//     .then(rows => {
+//         var row = rows[0];
+//         console.log(JSON.stringify(row));
+//     })
+//     ;
+
 
 const app = express()
 app.get('/', (request, response) => {
@@ -10,6 +48,13 @@ app.get('/', (request, response) => {
         myDbPeers: berlioz.getPeers('service', 'db', 'client')
     }
     response.send(data);
+})
+
+app.get('/entries', (request, response) => {
+    return executeQuery('select * from addressBook')
+        .then(rows => {
+            response.send(rows);
+        })
 })
 
 app.listen(process.env.BERLIOZ_LISTEN_PORT_CLIENT,
---
+++ 01.HelloWorld/v5.db/db/scripts/init.sql
@@ -0,0 +1,4 @@
+CREATE TABLE addressBook(name VARCHAR(20), phone VARCHAR(20));
+
+INSERT INTO addressBook(name, phone)
+VALUES ('Chuck', '123-456-7890');
