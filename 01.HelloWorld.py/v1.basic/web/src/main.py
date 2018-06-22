69d88679fb2010f92d33d0fe1dfcb26a02ed07ac
berlioz <berlioz-cloud@gmail.com>
2018-06-22 15:11:50 -0700

    templatized v1.basic python sample

--- 01.HelloWorld.py/v1.basic/web/src/main.py
+++ 01.HelloWorld.py/v1.basic/web/src/main.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
+
+import os
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized'
+    settings = [
+        {'name': 'Task ID', 'value': os.environ.get('BERLIOZ_TASK_ID') },
+        {'name': 'Instance ID', 'value': os.environ.get('BERLIOZ_INSTANCE_ID') },
+        {'name': 'Region', 'value': os.environ.get('BERLIOZ_REGION') },
+        {'name': 'Cluster', 'value': os.environ.get('BERLIOZ_CLUSTER') },
+        {'name': 'Service', 'value': os.environ.get('BERLIOZ_SERVICE') }
+    ]
+    return render_template('index.html', settings=settings)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
