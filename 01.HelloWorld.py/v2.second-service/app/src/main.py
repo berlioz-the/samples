2cbef7bbb9ce4c8340758f5f09f58c9fb6174ecb
berlioz <berlioz-cloud@gmail.com>
2018-06-23 14:02:05 -0700

    Added two server python hello world example.

--- 01.HelloWorld.py/v2.second-service/app/src/main.py
+++ 01.HelloWorld.py/v2.second-service/app/src/main.py
@@ -1,12 +1,18 @@
 from flask import Flask
 app = Flask(__name__)
 
+import os
+from flask import json
+
 import berlioz
 berlioz.setupFlask(app)
 
 @app.route('/')
 def my_handler():
-    return '<b>FROM APP</b>'
+    return json.dumps({
+        'message': 'Hello from App Tier',
+        'appId': os.environ['BERLIOZ_TASK_ID']
+    })
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
