4fc3624790603d84ddbf51fe0510ccadf91e69b8
berlioz <berlioz-cloud@gmail.com>
2018-09-13 22:13:21 -0700

    lambda sample now has api gateway endpoint

--- 07.Lambda/dosomething/src/index.js
+++ 07.Lambda/dosomething/src/index.js
@@ -1,4 +1,10 @@
 exports.handler = function(event, context, callback) {
-    console.log('HELLO WORLD!!!')    
-    callback(null, 'HELLO!!')
+    console.log('HELLO WORLD!!!') 
+    var res = {
+        headers: {
+        },
+        statusCode: 200
+    }
+    res.body = JSON.stringify({message: "HELLO!!!!!!!"});
+    callback(null, res);
 }
