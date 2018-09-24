9c5acab985e014ac817cd332ec960008bf23c23e
berlioz <berlioz-cloud@gmail.com>
2018-09-23 21:10:04 -0700

    minor changes.

--- 07.Lambda/preToken/src/index.js
+++ 07.Lambda/preToken/src/index.js
@@ -1,10 +1,15 @@
-exports.handler = function(event, context, callback) {
-    console.log('PRE TOKEN!!!') 
-    var res = {
-        headers: {
-        },
-        statusCode: 200
-    }
-    res.body = JSON.stringify({message: "PRE TOKEN!!!!!!!"});
-    callback(null, res);
-}
+exports.handler = (event, context, callback) => {
+    console.log(event);
+    
+    event.response = {
+        claimsOverrideDetails: {
+            claimsToAddOrOverride: {
+                'AAAA': '11111'
+            
+            }
+        }
+    };
+    
+    // Return result to Amazon Cognito
+    context.done(null, event);    
+};
