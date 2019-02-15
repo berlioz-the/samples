57a824912706e594505dcdea3be705ebcf3c360c
Ruben Hakopian <ruben.hakopian@gmail.com>
2019-02-14 22:29:38 -0800

    added custom entry point to mysql docker file

--- 01.HelloWorld.java/v3.db-polyglot/db/docker-entrypoint-berlioz.sh
+++ 01.HelloWorld.java/v3.db-polyglot/db/docker-entrypoint-berlioz.sh
@@ -3,5 +3,9 @@
 echo "RUNNING: ls -la /var/lib/mysql"
 ls -la /var/lib/mysql
 
-echo "RUNNING ORIGINAL docker-entrypoint.sh"
-/usr/local/bin/docker-entrypoint.sh
+# FOR DEMO PURPOSES
+# echo "RUNNING: rm -rf /var/lib/mysql/*"
+# rm -rf /var/lib/mysql/*
+
+echo "RUNNING ORIGINAL docker-entrypoint.sh $@"
+/usr/local/bin/docker-entrypoint.sh mysqld $@
