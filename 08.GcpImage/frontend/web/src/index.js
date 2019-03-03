4a0849fa75e60dc179d1743da8407bf3030e9c06
berlioz <berlioz-cloud@gmail.com>
2019-03-02 18:24:01 -0800

    added basic file render

--- 08.GcpImage/frontend/web/src/index.js
+++ 08.GcpImage/frontend/web/src/index.js
@@ -1,8 +1,9 @@
 const _ = require('lodash');
+const Promise = require('the-promise');
 const express = require('express');
 const berlioz = require('berlioz-sdk');
 const {Storage} = require('@google-cloud/storage');
-const PubSub = require('@google-cloud/pubsub');
+const formidable = require('formidable');
 
 const app = express();
 berlioz.setupExpress(app);
@@ -13,30 +14,25 @@ app.set('view engine', 'ejs')
 
 app.get('/', function (req, response) {
     var renderData = {
-        settings: [
-            {name: 'Identity', value: berlioz.identity },
-            {name: 'Task ID', value: process.env.BERLIOZ_TASK_ID },
-            {name: 'Instance ID', value: process.env.BERLIOZ_INSTANCE_ID },
-            {name: 'Region', value: process.env.BERLIOZ_REGION },
-            {name: 'Cluster', value: process.env.BERLIOZ_CLUSTER },
-            {name: 'Service', value: process.env.BERLIOZ_SERVICE }
-        ],
+        images: []
     };
-    for(var x of _.keys(process.env)) {
-        renderData.settings.push({
-            name: 'ENV:: ' + x,
-            value: process.env[x]
-        })
-    }
 
     return Promise.resolve()
-        .then(() => berlioz.database('images').client(Storage).getFiles())
+        .then(() => berlioz.database('images').client(Storage).getFiles({ prefix: 'orig/'}))
         .then(result => {
-            var names = result.map(x => x.name);
-            renderData.settings.push({
-                name: 'RESULT:: ',
-                value: JSON.stringify(names)
-            })
+            for(var file of result)
+            {
+                var parts = file.name.split('/');
+                var name = _.last(parts);
+                if (!name) {
+                    continue;
+                }
+                renderData.images.push({
+                    name: name,
+                    origUrl: '/image/' + name,
+                    processedUrl: '/image/' + name
+                });
+            }
         })
         .catch(reason => {
             renderData.settings.push({
@@ -46,6 +42,59 @@ app.get('/', function (req, response) {
         })
         .then(() => response.render('pages/index', renderData));
 })
+
+app.get('/image/:id', function (req, response) {
+    console.log('***********************');
+    console.log('Param: ' + req.params.id);
+    return berlioz.database('images').client(Storage).file('orig/' + req.params.id)
+        .then(file => {
+            console.log('--------------------');
+            console.log(file);
+            return file.createReadStream();
+        })
+        .then(stream => {
+            stream.on('end', () => {
+                response.end();
+            });
+            stream.on('error', (error) => {
+                console.log(error);
+                response.end();
+            });
+            stream.pipe(response);
+        })
+        .catch(reason => {
+            console.log(reason);
+            response.send("error: " + reason.message);
+        });
+})
+
+app.post('/upload', (req, res) => {
+    var files = [];
+    new formidable.IncomingForm().parse(req)
+        .on('file', (name, file) => {
+            files.push(file);
+            console.log(file);
+        })
+        .on('aborted', () => {
+            console.error('Request aborted by the user')
+        })
+        .on('error', (err) => {
+            console.error('Error', err)
+        })
+        .on('end', () => {
+            return Promise.serial(files, file => {
+                res.write(`X File: ${file.name}, size: ${file.size}<br/>`);
+            })
+            .then(() => {
+                res.end()
+            })
+            .catch(reason => {
+                res.write('Error: ' + reason.message);
+                res.end()
+            })
+        })
+})
+
 
 for(var x of _.keys(process.env)) {
     console.log('ENV:: ' + x + ' => ' + process.env[x]);
