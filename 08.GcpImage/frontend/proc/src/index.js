e7e96dd4cea7a1a7fb99b537580a3c3208ce3c4e
berlioz <berlioz-cloud@gmail.com>
2019-03-03 16:30:56 -0800

    working on gcp image demo

---
+++ 08.GcpImage/frontend/proc/src/index.js
@@ -0,0 +1,131 @@
+const _ = require('lodash');
+const Promise = require('the-promise');
+const berlioz = require('berlioz-sdk');
+const {Storage} = require('@google-cloud/storage');
+const PubSub = require('@google-cloud/pubsub');
+
+berlioz.addon(require('berlioz-gcp'));
+
+var SubscriptionId = null;
+
+berlioz.queue('jobs').monitorFirst(peer => {
+    console.log('[Pub /Sub] Changed.');
+    console.log(peer)
+    if (peer) {
+        var name = peer.name;
+        name = name.replace('/topics/', '/subscriptions/');
+        name = name + '-' + berlioz.identity;
+        var msgRequest = {
+            name: name,
+            topic: peer.name
+        }
+        console.log('CREATING SUBSCRIPTION:');
+        console.log(msgRequest);
+        return berlioz.queue('jobs').client(PubSub, 'SubscriberClient').createSubscription(msgRequest)
+            .then(result => {
+                console.log('NEW SUBSCRIPTION RESULT:');
+                console.log(result);
+                SubscriptionId = result.name;
+            })
+            .catch(reason => {
+                if(reason.code == 6) {
+                    console.log('SUBSCRIPTION ALREADY PRESENT.');
+                    SubscriptionId = name;
+                } else {
+                    console.log('ERROR CREATING SUBSCRIPTION:');
+                    console.log(reason);
+                    throw reason;
+                }
+            });
+    } else {
+        SubscriptionId = null;
+    }
+})
+
+function processQueue()
+{
+    console.log('[processQueue] ...')
+    return Promise.resolve()
+        .then(() => {
+            if (!SubscriptionId) {
+                return Promise.timeout(5000); 
+            } else {
+                return processSubscription(SubscriptionId)
+            }
+        })
+        .then(() => processQueue());
+}
+
+function processSubscription(id)
+{
+    console.log('[processSubscription] %s...', id)
+
+    var pullRequest = {
+        subscription: id,
+        maxMessages: 5
+    }
+    return berlioz.queue('jobs').client(PubSub, 'SubscriberClient').pull(pullRequest)
+        .then(responses => {
+            console.log(responses);
+            return Promise.serial(responses.receivedMessages, x => processMessage(x));
+        })
+        .catch(reason => {
+            console.log('[processSubscription] Error: ');
+            console.log(reason);
+        })
+}
+
+function processMessage(message)
+{
+    console.log('[processMessage] ', message)
+    var data = JSON.parse(message.message.data.toString());
+    console.log('[processMessage] ', data)
+    return downloadImage(data.name)
+        .then(buf => {
+            console.log('[processMessage] got buffer: %s', buf.length)
+        })
+        .catch(reason => {
+            console.log('[processMessage] error in download: ')
+            console.log(reason);
+        })
+}
+
+function downloadImage(id)
+{
+    return new Promise((resolve, reject) => {
+        berlioz.database('images').client(Storage).file('orig/' + id)
+            .then(file => {
+                return file.createReadStream();
+            })
+            .then(stream => {
+                var bufs = [];
+                stream.on('data', d => {
+                    bufs.push(d);
+                });
+                stream.on('end', () => {
+                    var buf = Buffer.concat(bufs);
+                    resolve(buf)
+                });
+                stream.on('error', (error) => {
+                    console.log(error);
+                    reject(error);
+                });
+            })
+            .catch(reason => {
+                console.log(reason);
+                reject(reason);
+            });
+
+    });
+    
+}
+
+
+return processQueue()
+    .then(result => {
+        console.log("FINISHED: " + result);
+    })
+    .catch(reason => {
+        console.log("ERROR: ");
+        console.log(reason);
+    })
--- 08.GcpImage/frontend/web/src/index.js
+++ 08.GcpImage/frontend/web/src/index.js
@@ -1,9 +1,12 @@
 const _ = require('lodash');
 const Promise = require('the-promise');
+const fs = require('fs');
 const express = require('express');
 const berlioz = require('berlioz-sdk');
 const {Storage} = require('@google-cloud/storage');
+const PubSub = require('@google-cloud/pubsub');
 const formidable = require('formidable');
+const uuid = require('uuid/v4');
 
 const app = express();
 berlioz.setupExpress(app);
@@ -17,39 +20,70 @@ app.get('/', function (req, response) {
         images: []
     };
 
-    return Promise.resolve()
-        .then(() => berlioz.database('images').client(Storage).getFiles({ prefix: 'orig/'}))
-        .then(result => {
-            for(var file of result)
-            {
-                var parts = file.name.split('/');
-                var name = _.last(parts);
-                if (!name) {
-                    continue;
-                }
-                renderData.images.push({
+    return queryImages()
+        .then(names => {
+            return Promise.serial(names, name => {
+                var imageInfo = {
                     name: name,
-                    origUrl: '/image/' + name,
-                    processedUrl: '/image/' + name
-                });
-            }
+                    origUrl: '/image/' + name
+                };
+                return checkImageProcessed(name)
+                    .then(isProcessed => {
+                        if (isProcessed) {
+                            imageInfo.processedUrl = '/procimage/' + name;
+                        }
+                    })
+                    .then(() => {
+                        renderData.images.push(imageInfo);
+                    });
+            })
         })
         .catch(reason => {
-            renderData.settings.push({
-                name: 'ERROR:: ',
-                value: reason.message
-            })
+            renderData.error = reason.message;
         })
         .then(() => response.render('pages/index', renderData));
 })
 
 app.get('/image/:id', function (req, response) {
-    console.log('***********************');
-    console.log('Param: ' + req.params.id);
-    return berlioz.database('images').client(Storage).file('orig/' + req.params.id)
+    downloadImage('orig', req, response);
+})
+
+app.get('/procimage/:id', function (req, response) {
+    downloadImage('processed', req, response);
+})
+
+function queryImages()
+{
+    return berlioz.database('images').client(Storage).getFiles({ prefix: 'orig/'})
+        .then(files => {
+            return files.map(file => {
+                var parts = file.name.split('/');
+                var name = _.last(parts);
+                if (!name) {
+                    return null;
+                }
+                return name;
+            })
+        })
+        .then(names => names.filter(x => x));
+}
+
+function checkImageProcessed(name)
+{
+    return berlioz.database('images').client(Storage).file('processed/' + name)
+        .then(file => file.exists())
+        .then(result => {
+            if (result[0]) {
+                return true;
+            }
+            return false;
+        })
+}
+
+function downloadImage(kind, req, response)
+{
+    return berlioz.database('images').client(Storage).file(kind + '/' + req.params.id)
         .then(file => {
-            console.log('--------------------');
-            console.log(file);
             return file.createReadStream();
         })
         .then(stream => {
@@ -66,14 +100,13 @@ app.get('/image/:id', function (req, response) {
             console.log(reason);
             response.send("error: " + reason.message);
         });
-})
+}
 
 app.post('/upload', (req, res) => {
     var files = [];
     new formidable.IncomingForm().parse(req)
         .on('file', (name, file) => {
             files.push(file);
-            console.log(file);
         })
         .on('aborted', () => {
             console.error('Request aborted by the user')
@@ -82,22 +115,58 @@ app.post('/upload', (req, res) => {
             console.error('Error', err)
         })
         .on('end', () => {
+            var name = uuid();
             return Promise.serial(files, file => {
-                res.write(`X File: ${file.name}, size: ${file.size}<br/>`);
+                var stream = fs.createReadStream(file.path);
+                return uploadImage('orig', name, stream);
             })
             .then(() => {
-                res.end()
+                return publishJob({ name: name });
+            })
+            .then(() => {
+                res.redirect('/');
             })
             .catch(reason => {
+                console.log(reason);
                 res.write('Error: ' + reason.message);
                 res.end()
             })
         })
-})
+});
 
+function uploadImage(kind, name, stream)
+{
+    return new Promise((resolve, reject) => {
+        berlioz.database('images').client(Storage).file(kind + '/' + name)
+            .then(file => {
+                return file.createWriteStream();
+            })
+            .then(writeStream => {
+                stream.pipe(writeStream)
+                    .on('error', (error) => {
+                        reject(error);
+                    })
+                    .on('finish', () => {
+                        resolve();
+                    });
+            })
+            .catch(reason => {
+                reject(reason);
+            });
+    });
+}
 
-for(var x of _.keys(process.env)) {
-    console.log('ENV:: ' + x + ' => ' + process.env[x]);
+function publishJob(msg)
+{
+    const msgRequest = {
+        messages: [
+            {
+                data: Buffer.from(JSON.stringify(msg))
+            }
+        ],
+    };
+    return berlioz.queue('jobs').client(PubSub, 'PublisherClient')
+        .publish(msgRequest);
 }
 
 app.listen(process.env.BERLIOZ_LISTEN_PORT_DEFAULT,
