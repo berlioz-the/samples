513c901a68babdc54d4a7b036110a5d113ce6313
berlioz <berlioz-cloud@gmail.com>
2019-03-26 18:28:52 -0700

    moved samples to seaprate repo

--- 10.GCP/02.K8sAddr/book/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS demo;
-
-USE demo;
-
-CREATE TABLE IF NOT EXISTS contacts (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name varchar(100),
-    phone varchar(100)
-    );
